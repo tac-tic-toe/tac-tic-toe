c07b1d00440816d34328d24a71a2bdbab7bef622
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-07-17 13:34:37 +0200

    desktop: specify common dependency

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -10,41 +10,41 @@ group = "com.toleno"
 version = "1.0"
 
 repositories {
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+	mavenCentral()
+	maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
-    }
-    sourceSets {
-        named("jvmMain") {
-            dependencies {
-                implementation(project(":common"))
-                implementation(compose.desktop.currentOs)
-            }
-        }
-        named("jvmTest") {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-    }
+	jvm {
+		compilations.all {
+			kotlinOptions.jvmTarget = "1.8"
+		}
+		testRuns["test"].executionTask.configure {
+			useJUnitPlatform()
+		}
+	}
+	sourceSets {
+		named("jvmMain") {
+			dependencies {
+				implementation(project(":common:client"))
+				implementation(compose.desktop.currentOs)
+			}
+		}
+		named("jvmTest") {
+			dependencies {
+				implementation(kotlin("test"))
+			}
+		}
+	}
 }
 
 compose.desktop {
-    application {
-        mainClass = "MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "jvm"
-            packageVersion = "1.0.0"
-        }
-    }
+	application {
+		mainClass = "MainKt"
+		nativeDistributions {
+			targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+			packageName = "jvm"
+			packageVersion = "1.0.0"
+		}
+	}
 }
