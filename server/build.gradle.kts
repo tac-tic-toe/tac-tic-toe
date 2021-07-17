909bbac6cd821fa9daf56abfdf3944c3d199fc12
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-07-17 13:27:29 +0200

    server: add compose

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,10 +2,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
+    id("org.jetbrains.compose")
 }
 
 group = "com.toleno"
 version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
 
 dependencies {
     implementation(project(":server:database"))
