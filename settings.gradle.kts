e04ae86bb90385c7395945f4fedc9fa506745eb3
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-07-17 13:30:02 +0200

    include both commons in gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,9 @@
 rootProject.name = "tac-tic-toe"
 
-
 include(":android")
 include(":desktop")
-include(":common")
+include(":common:all")
+include(":common:client")
 include(":web")
 include(":server:database")
 include(":server")
-
