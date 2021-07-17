89aae5f7af2006a02031301e441673cafb6fec85
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-07-17 13:29:33 +0200

    buildSrc: remove unnecessary gradle build and settings declarations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,3 @@
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        classpath("com.android.tools.build:gradle:4.1.2")
-    }
-}
-
-group = "com.toleno"
-version = "1.0"
-
 allprojects {
     repositories {
         mavenCentral()
